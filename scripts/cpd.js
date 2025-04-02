3ac3837ee496df5abbf3451876012baeb2e540c1
NPHRRL <NPHRRLC@gmail.com>
2025-04-02 18:23:38 +0300

    Add files via upload

---
+++ scripts/cpd.js
@@ -0,0 +1,56 @@
+
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentSlide = 0;
+
+function showSlide(n) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+prevBtn.addEventListener('click', () => {
+    showSlide(currentSlide - 1);
+});
+
+nextBtn.addEventListener('click', () => {
+    showSlide(currentSlide + 1);
+});
+
+setInterval(() => {
+    showSlide(currentSlide + 1);
+}, 5000);
+
+showSlide(0);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamMenu = document.querySelector(".ham-menu");
+  const offScreenMenu = document.querySelector(".off-screen-menu");
+  
+  hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+  });
+  });
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".slider-container");
+  
+    function checkScroll() {
+      elements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+  
+        if (elementTop < windowHeight - 150) {
+          element.classList.add("show");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
+  });
+  
---
+++ scripts/home-page.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+});
